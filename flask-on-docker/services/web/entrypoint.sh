51a8f590baa313451b4c354cd95031f5e2ccdcbd
ACortex <53233006+ArturCortez@users.noreply.github.com>
2023-04-04 17:21:58 +0000

    edited Dockerfile

--- flask-on-docker/services/web/entrypoint.sh
+++ flask-on-docker/services/web/entrypoint.sh
@@ -4,11 +4,11 @@ if ["$DATABASE" = "postgres" ]
 then 
     echo "Waiting for Postgres..."
 
-while ! nc -z $SQL_HOST $SQL_PORT; do
-    sleep 0.1
-done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
 
-echo "Postgres Started"
+    echo "Postgres Started"
 
 fi
 
